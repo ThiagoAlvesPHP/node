ab9af4beabdef0a8f4db285f84fce7701c6669ce
Alves <thiagoalves@ltdeveloper.com.br>
2024-07-05 15:51:20 -0300

    feature:  create routes

--- src/server.ts
+++ src/server.ts
@@ -7,11 +7,31 @@ const port = 3000;
 
 app.use(helmet());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/', (req, res) => {
-    res.json({ message: 'Hello World!' });
+    res.json({message: 'Hello World!'});
+});
+
+app.get('/ping', (req, res) => {
+    res.json({message: 'Pong!'});
+});
+
+app.get('/products', (req, res) => {
+    res.json({products: ['Camiseta', 'Calça', 'Sapato']});
+});
+
+// exemplo de rota dinamica http://localhost:3000/voos/x/y
+app.get('/voos/:from/:to', (req, res) => {
+    let {from, to} = req.params;
+
+    res.json({
+        flight: {
+            from: from.toUpperCase(),
+            to: to.toUpperCase()
+        }
+    });
 });
 
 app.listen(port, () => {
